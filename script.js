17609b786e042e93c3daa2843488853204ef7bfc
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2021-03-23 21:12:30 -0300

    commit
    Finalizando...

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 let video = document.getElementById('meuVideo')
+
 let botaoPlay = document.getElementById('botaoPlay')
 let botaoPause = document.getElementById('botaoPause')
+
 let somTrue = document.getElementById('somTrue')
 let somFalse = document.getElementById('somFalse')
+
 let botoes = document.getElementById('botoes')
 let container = document.getElementById('container')
-let clicou = false
+
+let barraProgresso
+let update
+
 let mutou = true
+
+window.onload = iniciar()
+
 botoes.style.display = 'none'
 
+function iniciar(){
+    barraProgresso = document.getElementById('barraProgresso')
+    duracaoTotal = video.duration
+
+    update = setInterval(atualizaBarra, 500)
+}
+
+function atualizaBarra(){
+    if (!video.paused){
+        var resultado = (208.050794 * video.currentTime) / video.duration
+        barraProgresso.style.width = parseInt(resultado) + "px"
+    }
+}
+
 container.onmouseover = function(){
-    video.style.filter = 'opacity(60%)'
+    video.style.filter = 'opacity(50%)'
     botoes.style.display = 'initial'
 }
 
 container.onmouseout = function(){
     video.style.filter = 'opacity(100%)'
     botoes.style.display = 'none'
-}
-
-function clicarVideo(){
-    if (clicou == false){
-        video.pause()
-        clicou = true
-    }else{
-        video.play()
-        clicou = false
-    }
 }
 
 function play(){
@@ -45,14 +58,6 @@ function voltar(){
     video.pause()
     video.currentTime = 0
     video.play()
-}
-
-function mais(){
-    video.playbackRate += 0.10
-}
-
-function menos(){
-    video.playbackRate -= 0.10
 }
 
 function mais10(){
@@ -76,3 +81,15 @@ function mutado(){
         somFalse.style.display = 'initial'
     }
 }
+
+// FUNÇÃO COMENTADA BONUS: Caso queira fazer o video pausar ou dar play ao clicar no video :P
+// let clicou = false
+// function clicarVideo(){
+//     if (clicou == false){
+//         video.pause()
+//         clicou = true
+//     }else{
+//         video.play()
+//         clicou = false
+//     }
+// }
